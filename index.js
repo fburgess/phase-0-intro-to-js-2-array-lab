5abc8d2f41e30acc7d490e5db5ca98f6b6a0c1e1
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-01-20 22:49:53 -0500

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+const cats = []; 
+    cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+   const newCats= [
+       ...cats,
+       name
+   ]
+
+   return newCats;
+}
+
+function prependCat(name) {
+    const oldCats=[
+        name,...cats
+    ]
+    return oldCats;
+}
+
+function removeLastCat() {
+    const finalCat=[
+        ...cats.slice(0,2)
+    ]
+    return finalCat;
+}
+
+function removeFirstCat() {
+    const lastCat=[
+        ...cats.slice(1)
+    ]
+    return lastCat;
+}
